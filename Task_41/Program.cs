1d44c7b91d7893fdabc344899746008e5ff7d1ee
ruslan brazhnikov <ruslan.1995mai@gmail.com>
2022-08-26 14:57:23 +0500

    Task1

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите элементы(через пробел): ");
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+//Array.ConvertAll - преобразует один тип массива в другой.
+//.Split() - используется для разбиения на подстроки.
+
+int n = 0;
+
+int CountGreaterThanZero(int n)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            n++;
+        }
+    }
+    return n;
+}
+
+int res = CountGreaterThanZero(n);
+Console.WriteLine($"Кол-во элементов > 0 = {res}!");
